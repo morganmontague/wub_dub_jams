d9f1d7637b5279a6251eda43ac8875d0a5826675
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-11-16 15:01:12 +0000

    added playlist model

--- musicLibrary/music/models.py
+++ musicLibrary/music/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Playlist(models.Model):
+    genre_title = models.CharField(max_length=40)
+    def __str__(self):
+        return self.title
+
 class Genre(models.Model):
     genre_title = models.CharField(max_length=40)
     def __str__(self):
