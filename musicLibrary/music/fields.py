d6f0d1e84492614d498bb1568ee580340036c0cf
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-11-18 14:45:24 +0000

    new additions to try out different fields

--- musicLibrary/music/fields.py
+++ musicLibrary/music/fields.py
@@ -1,26 +1,38 @@
 from rest_framework import serializers
 from .models import *
 
+
+
 class albumfield(serializers.RelatedField):
     def to_representation(self, instance):
-        return instance.title
+        return instance.album_title
 
     def to_internal_value(self, data):
-        return Album.objects.get(name=data)
+        return Album.objects.get(album_title=data)
+
+
 
 class artistfield(serializers.RelatedField):
     def to_representation(self, instance):
-        return instance.title
+        return instance.artist_title
 
     def to_internal_value(self, data):
-        return Artist.objects.get(name=data)
+        return Artist.objects.get(artist_title=data)
+
+
 
 class playlistfield(serializers.RelatedField):
     def to_representation(self, instance):
-        return instance.title
+        return instance.playlist_title
 
     def to_internal_value(self, data):
-        return Playlist.objects.get(name=data)
+        return Playlist.objects.get(playlist_title=data)
+
+
+
+# def to_internal_value(self, data):
+#         obj, created = Album.objects.get_or_create(**data)
+#         return obj
 
 # class genrefield(serializers.RelatedField):
 #     def to_representation(self, instance):
